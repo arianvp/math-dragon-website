5d0e5821f1419fc916447d72df04d4d4a5f70917
Arian van Putten <aeroboy94@gmail.com>
2013-12-11 14:05:18 +0100

    hele hoop shit geregeld

--- js/site.js
+++ js/site.js
@@ -42,31 +42,33 @@ $(function() {
 
 
 $(function() {
-  var names = ['arian','mark', 'glenn', 'tim', 'ivor', 'bas', 'folkert'];
-  names.forEach(function(name){
-    $('.about-item.'+name).click(function(e){
-      var self = $('.foldout.'+name);
-      var hidden = self.hasClass('hidden');
-      if (hidden) {
-        $('.foldout').addClass('hidden');
-        self.removeClass('hidden');
-        var height = self.outerHeight();
-        self.height(0);
-        self.animate({'height': height}, 200);
-        $('.lower').animate({'padding-top': height  }, 200);
-        $('.arrows .arrow').addClass('hidden');
-        $('.arrows .'+name +' .arrow').removeClass('hidden');
-      } else {
-        $('.lower').animate({'padding-top': 0}, 200);
-        $('.foldout').addClass('hidden');
-        $('.arrows .'+name +' .arrow').addClass('hidden');
-        
+  if ($(window).width() > 990) {
+    var names = ['arian','mark', 'glenn', 'tim', 'ivor', 'bas', 'folkert'];
+    names.forEach(function(name){
+      $('.about-item.'+name).click(function(e){
+        var self = $('.foldout.'+name);
+        var hidden = self.hasClass('hidden');
+        if (hidden) {
+          $('.foldout').addClass('hidden');
+          self.removeClass('hidden');
+          var height = self.outerHeight();
+          self.height(0);
+          self.animate({'height': height}, 200);
+          $('.lower').animate({'padding-top': height  }, 200);
+          $('.arrows .arrow').addClass('hidden');
+          $('.arrows .'+name +' .arrow').removeClass('hidden');
+        } else {
+          $('.lower').animate({'padding-top': 0}, 200);
+          $('.foldout').addClass('hidden');
+          $('.arrows .'+name +' .arrow').addClass('hidden');
 
-        self.addClass('hidden');
 
-      }
+          self.addClass('hidden');
+
+        }
+      });
     });
-  });
+  }
 });
 
 
