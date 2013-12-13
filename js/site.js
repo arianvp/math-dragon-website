2859b4eb6f9d3a65daef0007be5553f5c425efbf
Glenn Stewart <glennstewart95@hotmail.com>
2013-12-13 14:01:20 +0100

    paar bugs uit de menu balk gehaald

--- js/site.js
+++ js/site.js
@@ -29,12 +29,13 @@ $(function() {
       $('.nav > li.active').removeClass('active');
       parent.addClass('active');
     }
+	$('.navbar-collapse').removeClass('in').addClass('collapse');
   });
   $('.navbar-brand').click(function(e){
 
-    if ($(window).width() <= 768) {
+    //if ($(window).width() <= 768) {
       $('a[href*=#top]').click();
-    }
+    //}
     $('.nav > li.active').removeClass('active');
     $('.nav > li').first().addClass('active');
   });
